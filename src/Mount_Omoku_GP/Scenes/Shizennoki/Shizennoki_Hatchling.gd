a36f08528ef6130b483dffa9215fad54c173d4c3
Charles Walberg <charlesbwalberg@gmail.com>
2023-03-19 21:59:11 -0400

    dev

--- src/Mount_Omoku_GP/Scenes/Shizennoki/Shizennoki_Hatchling.gd
+++ src/Mount_Omoku_GP/Scenes/Shizennoki/Shizennoki_Hatchling.gd
@@ -1,19 +1,19 @@
 extends KinematicBody2D
 
 
-export (float, 1, 1000) var horizontal_speed: float = 50
-export (float, 1, 500) var vertical_speed: float = 50
-export (float, 0, 1000) var amplitude: float = 100
+export (float, 1, 1000) var horizontal_speed: float = 250
+export (float, 1, 500) var vertical_speed: float = 150
+export (float, 0, 1000) var amplitude: float = 30
 export (float, 0, 1000) var frequency: float = TAU
-export (float, 1, 100) var wavelength: float = TAU * 5 # = 10PI
-export (float, 1, 100) var wavenumber: float = TAU / wavelength # = k; TAU = 2PI
+export (float, 0, 3.14) var phase: float = 0 # phase constant 
+export (float, 1, 100) var wavelength: float = TAU # TAU = 2PI # = λ
+export (float, 1, 100) var wavenumber: float = TAU / wavelength # = k 
 
 
 var time : float = 0
 var directional_input = Vector2()
 var velocity = Vector2()
 var collision_results: KinematicCollision2D
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,6 +31,7 @@ func start(pos):
 func _process(delta):
 	directional_input = process_input()
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # PHYSICS
 func _physics_process(delta):
@@ -46,19 +47,19 @@ func _physics_process(delta):
 	# 		https://animations.physics.unsw.edu.au/jw/travelling_sine_wave.htm#phase
 	#		https://www.compadre.org/osp/EJSS/4470/255.htm (Advanced Questions)
 
-	# y(x,t) = A sin (k x - ω t) + vertical shift
-	# y'(x, t) = -Aω cos (k x - ω t) + vertical shift
+	# y(x,t) = A sin (k x - ω t + φ) 	# + vertical shift
+	# y'(x, t) = -Aω cos (k x - ω t + φ)	# + vertical shift
 	
 	# y = a length: the displacement (from origin) of the string the wave oscillates around
 	# x = also a length: the position along the string
 	# t = time
-	# y', x' = ???
-	# A = amplitude, k = wavenumber, ω = frequency
+	# A = amplitude, k = wavenumber, ω = frequency, φ = phase constant
 
-	velocity.x = directional_input.x * horizontal_speed * delta		# constant forward/backward speed
-	velocity.y = (-amplitude * frequency * cos(wavenumber * velocity.x - frequency * time) + (directional_input.y * vertical_speed)) * delta
+	# TODO: Accelerate up to top speed
+	velocity.x = directional_input.x * horizontal_speed	# constant forward/backward speed
+	velocity.y = -amplitude * frequency * cos(wavenumber * velocity.x - frequency * time + phase) + (directional_input.y * vertical_speed)
 
-	collision_results = move_and_collide(velocity)
+	collision_results = move_and_collide(velocity * delta)
 	
 	# if collision_results:
 	# 	TODO: Process collision
